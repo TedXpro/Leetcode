2ca445deb00e6e10888defcf359c504a6e4a07ca
Yohannes <71769245+TedXpro@users.noreply.github.com>
2023-12-31 06:33:44 +0300

    Time: 625 ms (5.13%), Space: 38.5 MB (36.54%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        set<string> memo;
+        for(vector<int> obs : obstacles){
+            memo.insert(to_string(obs[0]) + "," + to_string(obs[1]));
+        }
+
+        vector<vector<int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int x = 0, y = 0;
+        int currDir = 0;
+        int answer = 0;
+        for(int command : commands){
+            if(command == -1){
+                currDir++;
+                if(currDir == 4)
+                    currDir = 0;
+            }
+            else if(command == -2){
+                currDir--;
+                if(currDir == -1)
+                    currDir = 3;
+            }
+            else{
+                while(command-- > 0 && memo.find(to_string(x + directions[currDir][0]) +
+                 "," + to_string(y + directions[currDir][1])) == memo.end()){
+                    x += directions[currDir][0];
+                    y += directions[currDir][1];
+                }
+            }
+            cout << x << "," << y << endl;
+            answer = max(answer, x * x + y * y);
+        }
+        return answer;
+        
+    }
+};
