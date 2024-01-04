c1b5290881b806992df63c7b99d6b8c5381b6017
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-01-04 05:29:46 +0300

    Time: 108 ms (91.16%), Space: 85.2 MB (46.04%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int, int> occurence;
+        for(int num : nums)
+            occurence[num]++;
+        
+        int count = 0;
+        for(auto occ : occurence){
+            if(occ.second == 1)
+                return -1;
+            else if(occ.second % 3 == 1){
+                count += occ.second / 3 - 1;
+                count += 2;
+            }
+            else if(occ.second % 3 == 2){
+                count += occ.second / 3;
+                count += 1;
+            }   
+            else
+                count += occ.second / 3;
+        }
+        return count;
+    }
+};
