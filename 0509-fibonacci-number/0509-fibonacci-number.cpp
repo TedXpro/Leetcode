25633b2ee99522523018ace4a2bee1ec467078fb
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-02-06 08:02:41 +0300

    Time: 3 ms (60.96%), Space: 7.2 MB (10.71%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,39 @@
-class Solution {
+/* Top Down approach memoization */
+// class Solution {
+// private:
+//     unordered_map<int, int> memo;
+//     int findFib(int n){
+//         if(n == 0)
+//             return 0;
+//         if(n <= 2)
+//             return 1;
+//         if(memo.find(n) != memo.end())
+//             return memo[n];
+//         int result = findFib(n - 1) + findFib(n - 2);
+//         memo[n] = result;
+//         return result;
+//     }
+// public:
+//     int fib(int n) {
+//         return findFib(n);
+//     }
+// };
+
+/* Bottom Up approach */
+class Solution{
 public:
     int fib(int n) {
-        if(n == 0)
-            return 0;
-        else if(n == 1)
-            return 1;
-        return fib(n - 1) + fib(n - 2);
+        if(n <= 1)
+            return n;
+
+        int second = 0;
+        int first = 1;
+        for(int i = 1; i < n; i++){
+            int temp = first + second;
+            second = first;
+            first = temp;
+        }
+
+        return first;
     }
 };
