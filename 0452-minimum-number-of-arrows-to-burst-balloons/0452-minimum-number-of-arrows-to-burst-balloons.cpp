7621c864111125c1aafed229f7d11b6d0648b64c
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-03-18 06:05:32 +0300

    Time: 260 ms (51.47%), Space: 93.4 MB (41.78%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -4,8 +4,8 @@ public:
         int count = 1;
         sort(points.begin(), points.end());
         int startInterval = points[0][0], endInterval = points[0][1];
-        for(int i = 0; i < points.size(); i++){
-            if(points[i][0] >= startInterval && points[i][0] <= endInterval){
+        for (int i = 0; i < points.size(); i++) {
+            if (points[i][0] >= startInterval && points[i][0] <= endInterval) {
                 endInterval = min(endInterval, points[i][1]);
             } else {
                 startInterval = points[i][0];
