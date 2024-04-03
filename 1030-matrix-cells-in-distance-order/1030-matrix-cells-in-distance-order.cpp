8a0731f81b30f1ff3dde679fc427a114f87d12fd
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-04-03 04:12:14 +0300

    Time: 53 ms (29.45%), Space: 28.1 MB (33.97%) - LeetHub

---
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        vector<vector<int>> answer;
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                int distance = abs(row - rCenter) + abs(col - cCenter);
+                vector<int> ans = {distance, row, col};
+                answer.push_back(ans);
+            }
+        }
+
+        sort(answer.begin(), answer.end());
+        vector<vector<int>> result;
+        for(vector<int>& ans : answer){
+            result.push_back(vector<int>(ans.begin() + 1, ans.end()));
+        }
+        return result;
+    }
+};
