9cdee71a4c0bc7844e8b7d1381209c1356ea1625
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-04-18 17:00:50 +0300

    Time: 87 ms (8.05%), Space: 34.3 MB (5.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,6 +1,6 @@
 class Solution {
     void countIslands(vector<vector<char>> &grid, int row, int col, unordered_set<string> &memo){
-        if(row < 0 || row >= grid[0].size() || col < 0 || col >= grid[0].size() || grid[row][col] == '0')
+        if(row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() || grid[row][col] == '0')
             return;
         string key = to_string(row) + "," + to_string(col);
         if(memo.find(key) != memo.end())
@@ -8,10 +8,23 @@ class Solution {
         memo.insert(key);
         countIslands(grid, row - 1, col, memo);
         countIslands(grid, row, col - 1, memo);
-        
+        countIslands(grid, row + 1, col, memo);
+        countIslands(grid, row, col + 1, memo);
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        
+        unordered_set<string> memo;
+        int answer = 0;
+        for(int row = 0; row < grid.size(); row++){
+            for(int col = 0; col < grid[0].size(); col++){
+                string key = to_string(row) + "," + to_string(col);
+                if(grid[row][col] != '0' && memo.find(key) == memo.end()){
+                    answer++;
+                    countIslands(grid, row, col, memo);
+                }
+            }
+        }
+
+        return answer;
     }
 };
