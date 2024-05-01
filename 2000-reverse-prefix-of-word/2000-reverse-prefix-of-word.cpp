6495aa42b468038c17e9d2e10d9ed0f8867fedc8
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-05-01 08:15:53 +0300

    Time: 0 ms (100%), Space: 7.4 MB (91.39%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int index = -1;
-        for(int i = 0; i < word.size(); i++){
-            if(word[i] == ch){
-                index = i;
-                break;
-            }
-        }
+        int index = word.find(ch);
+        // for(int i = 0; i < word.size(); i++){
+        //     if(word[i] == ch){
+        //         index = i;
+        //         break;
+        //     }
+        // }
 
         if(index == -1)
             return word;
