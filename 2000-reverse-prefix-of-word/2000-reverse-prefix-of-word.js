88192b69e759d0fdb869456eb6f2f0f7990c2fe4
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-05-01 14:27:20 +0300

    Time: 57 ms (30.16%), Space: 48.5 MB (84.82%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function(word, ch) {
+    var index = word.indexOf(ch);
+    if(index === -1)
+        return word;
+    return word.substring(0, index + 1).split("").reverse().join("") + word.substring(index + 1);
+};
