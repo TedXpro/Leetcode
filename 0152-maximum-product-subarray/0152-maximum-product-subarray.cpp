948845695d95820c632fe6e0b1e8dbd4ca7fd3f8
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-06-02 17:27:23 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,15 +4,9 @@ public:
         int maximum = *max_element(nums.begin(), nums.end());
         int currMax = 1, currMin = 1;
         for(int n: nums){
-            if(n == 0) {
-                currMax = 1;
-                currMin = 1;
-            }
-
             int tmp = currMax * n;
-            currMax = max({currMax * n, currMin * n, n});
+            currMax = max({tmp, currMin * n, n});
             currMin = min({tmp, currMin * n, n});
-
             maximum = max({currMax, currMin, maximum});
         }
         
