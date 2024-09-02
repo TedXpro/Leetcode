85821dc1dbbf09b020315a12f8b105f5b7cf6d02
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-09-02 11:33:20 +0300

    Time: 72 ms (86.48%), Space: 91.5 MB (47.98%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         if(m == 1 && n == 1 && original.size() != 1){
-return {};}
+            return {};
+        }
         if(m * n != original.size()){
-return {};
-}
+            return {};
+        }
+        
         vector<vector<int>> answer;
         int curr = 0;
         vector <int> currVec;
