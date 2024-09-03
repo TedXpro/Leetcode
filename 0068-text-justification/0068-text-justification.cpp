0d34d3a946e3f337454889fee7b40df9f10bffef
Yohannes <71769245+TedXpro@users.noreply.github.com>
2024-09-03 10:36:08 +0300

    Time: 0 ms (100%), Space: 9.3 MB (72.44%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.cpp
@@ -0,0 +1,74 @@
+class Solution {
+    string construct(string& curr, int currLength, int maxWidth, int numWords, bool leftJustified){
+        if(leftJustified){
+            int addSpaces = maxWidth - currLength;;
+            for(int i = 0; i < addSpaces; i++){
+                curr += " ";
+            }
+            return curr.substr(0, maxWidth);
+        }
+        curr = curr.substr(0, curr.length() - 1);
+        int addSpaces = maxWidth - currLength;
+        int rem = addSpaces % (numWords - 1);
+        addSpaces = addSpaces / (numWords - 1);
+        string res = "";
+        for(char ch : curr){
+            if(ch == ' '){
+                res += ch;
+                for(int i = 0; i < addSpaces; i++){
+                    res += ch;
+                }
+                if(rem > 0){
+                    res += ch;
+                    rem--;
+                }
+            } else {
+                res += ch;
+            }
+        }
+        return res;
+    }
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        string curr = "";
+        int currLength = 0;
+        int numWords = 0;
+        int total = 0;
+        vector<string> answer;
+        bool inserted = false;
+        for(string &word : words){
+            if(currLength + word.length() > maxWidth){
+                cout << currLength << ": " << currLength + word.length() << " word: " << word << endl;
+                cout << "curr: " << curr << "!! " << curr.length() <<  endl;
+                if(numWords == 1){
+                    answer.push_back(construct(curr, currLength - 1, maxWidth, numWords, true));
+                }
+                else if(total == words.size()){
+                    inserted = true;
+                    answer.push_back(construct(curr, currLength - 1, maxWidth, numWords, true));
+                } else {
+                    answer.push_back(construct(curr, currLength - 1, maxWidth, numWords, false));
+                }
+
+                curr = word + " ";
+                currLength = word.length() + 1;
+                numWords = 1;
+            } else {
+                curr += word + " ";
+                numWords++;
+                currLength += word.length() + 1;
+                total++;
+            }
+        }
+
+        if(!inserted){
+            answer.push_back(construct(curr, currLength - 1, maxWidth, numWords, true));
+        }
+
+        return answer;
+    }
+};
+
+
+// ["This    is    an","example  of  text","justification.  "]
+// ["This    is    an","example  of text","justification.  "]
