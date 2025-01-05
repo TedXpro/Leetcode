15975902a059df9df9a592cdd4d5df867f60997c
Yohannes <71769245+TedXpro@users.noreply.github.com>
2025-01-05 11:36:18 +0300

    Time: 0 ms (100%), Space: 16.6 MB (48.99%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
@@ -4,22 +4,14 @@ public:
         unordered_map<char, int> memo;
         for(int i = 0; i < s.length(); i++){
             if(memo.find(s[i]) != memo.end()){
-                memo[s[i]] = i - memo[s[i]] - 1;
+                int curr = s[i] - 'a';
+                if(distance[curr] != (i - memo[s[i]] - 1)){
+                    return false;
+                }
             } else {
                 memo[s[i]] = i;
             }
         }
-
-        for(int i = 0; i < distance.size(); i++){
-            char curr = static_cast<char>(i + 'a');
-            cout << curr << endl;
-            if(memo.find(curr) != memo.end()){
-                if(distance[i] != memo[curr]){
-                    return false;
-                }
-            }
-        }
-
         return true;
     }
 };
